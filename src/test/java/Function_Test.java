2cf720268dbe943b9a22aa130c17783d618bbf82
Lunderosn <snaumochkin1@gmail.com>
2022-11-20 18:35:32 +0300

    Первый тест

--- src/test/java/Function_Test.java
+++ src/test/java/Function_Test.java
@@ -1,2 +1,22 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Function_Test {
+
+    Function function;
+    /**
+     *В данном методе мы создаем объект класса до начала тестирования, как указано в задании. Это делаем благодаря @Before.
+     *
+     * */
+    @Before
+    public void createObject(){
+        function = new Function();
+    }
+
+    @Test
+    public void functionResult(){
+        Assert.assertEquals(10.0, Function.func(10,1),0.5);
+    }
+
 }
